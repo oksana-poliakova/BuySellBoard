cc0a6bac119767d5c08b93fdde39dcec123dfc4e
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-01-12 14:06:05 +0200

    Initial commit

---
+++ src/main/java/com/example/buysellboard/BuySellBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.buysellboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySellBoardApplication.class, args);
+	}
+
+}

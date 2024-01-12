d3a6e12ec4482b4a3293e87f654a43da6a98cd3a
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2024-01-12 19:23:21 +0200

    Added getAllProducts(), deleteProductById() to the ProductController

--- src/main/java/com/example/buysellboard/controllers/ProductController.java
+++ src/main/java/com/example/buysellboard/controllers/ProductController.java
@@ -2,14 +2,19 @@ package com.example.buysellboard.controllers;
 
 import com.example.buysellboard.dtos.product.InsertProductDTO;
 import com.example.buysellboard.dtos.product.ProductDTO;
+import com.example.buysellboard.entities.Product;
 import com.example.buysellboard.services.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author oksanapoliakova on 12.01.2024
@@ -36,9 +41,29 @@ public class ProductController {
      * @param insertProductDTO The data transfer object containing information about the new product.
      * @return The ProductDTO representing the newly added product.
      */
-    @PostMapping("/add")
-    @Operation(summary = "Posibility to add a product to the database")
-    public ProductDTO save(@Valid @RequestBody InsertProductDTO insertProductDTO) {
+    @PostMapping("/addProduct")
+    @Operation(summary = "Possibility to add a product to the database")
+    public ProductDTO addProduct(@Valid @RequestBody InsertProductDTO insertProductDTO) {
         return productService.mapToDTO(productService.addProduct(insertProductDTO));
+    }
+
+    /**
+     * Controller method for handling GET requests to retrieve all products from the database.
+     * @return A List of Product entities representing all products in the database.
+     */
+    @GetMapping("/getAllProducts")
+    @Operation(summary = "Possibility to get all products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    /**
+     * Controller method for handling DELETE requests to delete a product from the database by its ID.
+     * @param product product The Product entity to be deleted from the database.
+     */
+    @DeleteMapping("/deleteById")
+    @Operation(summary = "Possibility to delete a product by id")
+    public void deleteProductById(Product product) {
+        productService.deleteProductById(product);
     }
 }
